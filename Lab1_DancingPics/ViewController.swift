7550533988a0070591f30b2e2dae050ac4d1f5c6
trje9656 mac <trje9656@colorado.edu>
2021-09-22 22:27:45 -0600

    Added Animation slider Capital text switch blue text color switch font size slider segment control image toggle images need for animation slider

--- Lab1_DancingPics/ViewController.swift
+++ Lab1_DancingPics/ViewController.swift
@@ -8,23 +8,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var changinglabel: UILabel!
     @IBOutlet weak var artImage: UIImageView!
-    @IBAction func chooseDance(_ sender: UIButton) {
-        if sender.tag == 1{
-            artImage.image = UIImage(named: "squat")
-            changinglabel.text = "yeah..."
-        }
-        else if sender.tag == 2{
-            artImage.image = UIImage(named: "headbob")
-            changinglabel.text = "Ohh..."
-        }
-        else if sender.tag == 3{
-            artImage.image = UIImage(named: "michael")
-            changinglabel.text = "Michael!"
-        }
+    @IBOutlet weak var imageControl: UISegmentedControl!
+    @IBOutlet weak var capitalSwitch: UISwitch!
+    @IBOutlet weak var animationSlider: UISlider!
+    @IBOutlet weak var colorSwitch: UISwitch!
     
+    
+    func updateImage(){
+        if imageControl.selectedSegmentIndex == 0 {
+            changinglabel.text="Michael!"
+            artImage.image=UIImage(named: "michael")
+        }
+        else if imageControl.selectedSegmentIndex == 1 {
+            changinglabel.text="Ohh Yea"
+            artImage.image=UIImage(named: "squat")
+        }
+    }
+    func updateColor() {
+        if colorSwitch.isOn {
+            changinglabel.textColor = UIColor.blue
+        } else {
+            changinglabel.textColor = UIColor.black
+        }
+    }
+    
+    func updateCaps(){
+        if capitalSwitch.isOn {
+            changinglabel.text=changinglabel.text?.uppercased()
+        } else {
+            changinglabel.text=changinglabel.text?.lowercased()
+        }
+    }
+    @IBAction func updateFont(_ sender: UISwitch) {
+        updateCaps()
+    }
+    @IBAction func changeFontSize(_ sender: UISlider) {
+        let fontSize=sender.value //float
+        
+        let fontSizeCGFloat=CGFloat(fontSize) //create a CGFloat from a float
+        changinglabel.font=UIFont.systemFont(ofSize: fontSizeCGFloat)
+    }
+    
+    @IBAction func updateDanceFrame(_ sender: UISlider) {
+        if imageControl.selectedSegmentIndex == 0 {
+            var frame = Int(sender.value) % 15
+            var framename = String("m-\(frame)" )
+            artImage.image=UIImage(named: framename)
+        }
+        else if imageControl.selectedSegmentIndex == 1 {
+            var frame = Int(sender.value) % 2 + 1
+            var framename = String("squat\(frame)" )
+            artImage.image=UIImage(named: framename)
+        }
+        
+        
+    }
+    @IBAction func changeColor(_ sender: UISwitch) {
+        updateColor()
+    }
+    @IBAction func chooseDance(_ sender: UISegmentedControl) {
+        updateImage()
+        updateCaps()
+        //        if sender.tag == 1{
+        //            artImage.image = UIImage(named: "squat")
+        //            changinglabel.text = "yeah..."
+        //        }
+        //        else if sender.tag == 2{
+        //            artImage.image = UIImage(named: "headbob")
+        //            changinglabel.text = "Ohh..."
+        //        }
+        //        else if sender.tag == 3{
+        //            artImage.image = UIImage(named: "michael")
+        //            changinglabel.text = "Michael!"
+        //        }
+        
     }
     
     
@@ -34,7 +94,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
